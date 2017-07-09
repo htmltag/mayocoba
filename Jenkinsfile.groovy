bd8c0c80fa88218cc160d6533f65276900c5d80e
htmltag <jonathansoylandlier@gmail.com>
2017-07-09 23:17:01 +0200

    vol

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,8 @@
 podTemplate(label: 'greenland-jenkins-slave', containers: [
-    containerTemplate(name: 'docker', image: 'docker:17.06-dind', ttyEnabled: true, command: 'cat', privileged: true, instanceCap: 1),
-    containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat')
-]) {
+    containerTemplate(name: 'docker', image: 'docker:17.06-dind', ttyEnabled: true, command: 'cat'),
+    containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat')],
+    volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]
+) {
 
 node ('greenland-jenkins-slave'){
 
